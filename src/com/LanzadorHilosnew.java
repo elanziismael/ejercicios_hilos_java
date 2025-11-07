b2cb4f631cba713efd7bdb6830ea732d80ffcd37
Ismael <elanziismael@gmail.com>
2025-11-07 20:00:38 +0100

    segundo

--- src/com/LanzadorHilosnew.java
+++ src/com/LanzadorHilosnew.java
@@ -2,9 +2,16 @@ package com;
 
 public class LanzadorHilosnew {
     public static void main(String[] args) {
-        Thread h = new Thread(new Hilo("nuevo"));
+        Thread h = new Thread(new Hilo("runnable"));
+        h.start();
+        try {
+            Thread.sleep(50);
+            System.out.println(h.getName() + " -> " + h.getState());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        System.out.println(h.getName() + " -> " + h.getState());
+        
 
 
     }
