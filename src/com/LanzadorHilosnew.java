ccbe2762fdb84aa9e20ef4fa297e84a35c17b9ee
Ismael <elanziismael@gmail.com>
2025-11-07 19:54:43 +0100

    correccion

--- src/com/LanzadorHilosnew.java
+++ src/com/LanzadorHilosnew.java
@@ -4,7 +4,7 @@ public class LanzadorHilosnew {
     public static void main(String[] args) {
         Thread h = new Thread(new Hilo("nuevo"));
 
-        System.out.println("🟡 Lanzando hilo: " + h.getName());
+        System.out.println(h.getName() + " -> " + h.getState());
 
 
     }
