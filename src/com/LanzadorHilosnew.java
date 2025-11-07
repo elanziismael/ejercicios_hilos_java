2c6cd24261f7bb2e8fd66030732a7349bb054b3c
Ismael <elanziismael@gmail.com>
2025-11-07 20:13:22 +0100

    cuarto ejercicio

--- src/com/LanzadorHilosnew.java
+++ src/com/LanzadorHilosnew.java
@@ -4,23 +4,17 @@ public class LanzadorHilosnew {
     private static final Object LOCK = new Object();
 
     public static void main(String[] args) throws InterruptedException {
-        Thread t1 = new Thread(() -> {
+        Thread t = new Thread(() -> {
             synchronized (LOCK) {
-                try { Thread.sleep(2000); } catch (InterruptedException ignored) {}
+                try {
+                    System.out.println(Thread.currentThread().getName() + " esperando...");
+                    LOCK.wait();
+                } catch (InterruptedException ignored) {}
             }
-        }, "t1");
-
-        Thread t2 = new Thread(() -> {
-            synchronized (LOCK) {
-                System.out.println("Bloqueado: " + Thread.currentThread().getName());
-            }
-        }, "t2");
-
-        t1.start();
+        }, "tWaiting");
+        t.start();
         Thread.sleep(100);
-        t2.start();
-        Thread.sleep(50);
-        System.out.println(t2.getName() + " -> " + t2.getState());
+        System.out.println(t.getName() + " -> " + t.getState());
     }
     
 }
