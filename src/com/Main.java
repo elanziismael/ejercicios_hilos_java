cc1a99eed0dd147c9113069e4454e09cd595b882
Ismael <elanziismael@gmail.com>
2025-11-14 19:47:00 +0100

    subida2

--- src/com/Main.java
+++ src/com/Main.java
@@ -3,11 +3,29 @@ public class Main{
 
     public static void main(String [] args) {
 
-		Thread hilo = new Thread(() -> {
-            System.out.println("Hola desde un hilo");
+		Thread hilo1 = new Thread(() -> {
+            String nombre = Thread.currentThread().getName();
+            System.out.println("Ejecutando: " + nombre);
         });
-        hilo.start();
-        System.out.println("Hola desde main");
+
+        hilo1.setName("Hilo-Usuario");
+
+        Thread hilo2 = new Thread(() -> {
+            String nombre = Thread.currentThread().getName();
+            System.out.println("Ejecutando: " + nombre);
+        });
+        hilo2.setName("Proceso-Background");
+
+        Thread hilo3 = new Thread(() -> {
+            String nombre = Thread.currentThread().getName();
+            System.out.println("Ejecutando: " + nombre);
+        });
+
+        hilo1.start();
+        hilo2.start();
+        hilo3.start();
+
+        System.out.println("Ejecutando: " + Thread.currentThread().getName());
 	}
 
 
